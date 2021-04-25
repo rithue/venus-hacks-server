965c30ec8ebe733e9c5e05328ba7886b050faf27
Rithu Eswaramoorthy <rithue2000@gmail.com>
2021-04-25 00:36:42 -0700

    Fix API call

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-let worker = require('../routes/worker');
+let worker = require("../routes/worker");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-router.get('/carbonContent', function(req,res) {
-  // worker.getCarbonContent(req.query.startZipCode,req.query.endZipCode,function(err,response){
-  //   res.send({"distance":"40.56", "carbonEmission":"1000"});
-  // });
-  res.send({"distance":"40.56", "carbonEmission":"1000"});
+router.get("/carbonContent", function (req, res) {
+  worker.getCarbonContent(
+    req.query.startZipCode,
+    req.query.endZipCode,
+    req.query.fuelType,
+    function (response) {
+      res.send(response);
+    }
+  );
 });
 
 module.exports = router;
--- routes/worker.js
+++ routes/worker.js
@@ -1,14 +1,84 @@
-const https = require('https');
+const https = require("https");
+const fetch = require("node-fetch");
 
-const ZIPCODE_URL = "https://www.zipcodeapi.com/rest/"
-const ZIPCODE_KEY = "lgPfA4rtwypjiXUlGZCkBeqcjkEitH65fetoBvU0Bri3zQd1lVUu7qdFKu6eHwR9"
+const ZIPCODE_URL = "https://www.zipcodeapi.com/rest/";
+const ZIPCODE_KEY =
+  "lgPfA4rtwypjiXUlGZCkBeqcjkEitH65fetoBvU0Bri3zQd1lVUu7qdFKu6eHwR9";
+const CARBON_INTERFACE_URL = "https://www.carboninterface.com/api/v1/estimates";
+const CARBON_INTERFACE_TOKEN = "wKeeJfodPXHcoYr3IOL04g";
+const fuelTypes = {
+  hybrid: "f9ff8de7-94a6-4a78-b70c-686dcbf720cc",
+  electric: "ab91dfaf-004d-49e4-95d0-edd052eb44e1",
+  gas: "7268a9b7-17e8-4c8d-acca-57059252afe9",
+};
 
+function getCarbonContent(startZipCode, endZipCode, fuelType, cb) {
+  const distance = getDistance(startZipCode, endZipCode, function (response) {
+    console.log("distance:", response.distance);
+    getCarbonInterface(response.distance, fuelType, function (resp) {
+      let result = {
+        distance: response.distance,
+        carbonEmission: resp.attributes.carbon_kg,
+      };
+      console.log("result:", result);
+      cb(result);
+    });
+  });
+}
 
-function getCarbonContent(startZipCode,endZipCode,cb) {
-    console.log(startZipCode+' '+endZipCode)
-    https.get(ZIPCODE_URL+ZIPCODE_KEY+`/distance.json/${startZipCode}/${endZipCode}/mile`,res => {
-        console.log(res)
-    })
+function getDistance(startZipCode, endZipCode, cb) {
+  https
+    .get(
+      ZIPCODE_URL +
+        ZIPCODE_KEY +
+        `/distance.json/${startZipCode}/${endZipCode}/mile`,
+      (res) => {
+        let body = "";
+        res.on("data", (chunk) => {
+          body += chunk;
+        });
+
+        res.on("end", () => {
+          try {
+            let json = JSON.parse(body);
+            console.log("json:", json);
+            console.log("json.distance:", json.distance);
+            cb(json);
+          } catch (error) {
+            console.error(error.message);
+          }
+        });
+      }
+    )
+    .on("error", (error) => {
+      console.error(error.message);
+    });
+}
+
+function getCarbonInterface(distance, fuelType, cb) {
+  let bearer = "Bearer " + CARBON_INTERFACE_TOKEN;
+
+  let requestPayload = {
+    type: "vehicle",
+    distance_unit: "mi",
+    distance_value: distance,
+    vehicle_model_id: fuelTypes[fuelType],
+  };
+  let settings = {
+    method: "POST",
+    headers: {
+      Authorization: bearer,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestPayload),
+  };
+
+  fetch(CARBON_INTERFACE_URL, settings)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log("CI:", json);
+      cb(json.data);
+    });
 }
 
 exports.getCarbonContent = getCarbonContent;
